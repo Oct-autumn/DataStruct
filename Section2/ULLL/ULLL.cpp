62c9b1ab24f954899570605e54810aa6cd766d0c
仕旻 张 <lljkiff@163.com>
2021-02-06 10:45:09 +0800

    Upload
    通用链表库
    (其实算不上通用，只是一种算法解决方案)

--- Section2/ULLL/ULLL.cpp
+++ Section2/ULLL/ULLL.cpp
@@ -1,20 +1,36 @@
-﻿// ULLL.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+﻿// 本文件为ULLL（全称Universal Linked List Library（通用链表库））的测试代码
 //
 
 #include <iostream>
+#include "ULLL.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    SinglyNode* Head = NULL;
+    SinglyNode* NowNode = NULL;
+
+    int a[10] = { 1, 5, 6, 3, 2, 7, 9, 0, 8, 4 };
+    
+    DataSpace* Buffer = (DataSpace*)malloc(sizeof(DataSpace));
+
+    Buffer->num = a[0];
+    NowNode = NewNode_Singly(*Buffer, NowNode);
+    Head = NowNode;
+
+    for (int i = 1; i < 10; i++)
+    {
+        Buffer->num = a[i];
+        NowNode = NewNode_Singly(*Buffer, NowNode);
+    }
+
+    SinglyListSort(Head, 10, MINtoMAX);
+
+    NowNode = Head;
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d\n", NowNode->Data.num);
+        NowNode = NowNode->next;
+    }
+
+    return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
--- Section2/ULLL/ULLL.h
+++ Section2/ULLL/ULLL.h
@@ -1,3 +1,5 @@
+//ULLL（全称Universal Linked List Library（通用链表库））
+
 #pragma once
 
 #include <stdio.h>
@@ -7,27 +9,38 @@
 #define MAXtoMIN 0
 #define MINtoMAX 1
 
+#define KEY_NUM(M) M->Data.num
+
+typedef struct DataSpace
+{
+	int num;
+	char str[20];
+};
+
+
 
 //以下为单向链表部分
 typedef struct SinglyNode	//单向链表节点
 {
 	//数据区（请在下面定义每个节点中的数据变量）
-	int num;
-
+	DataSpace Data;
 	//指向下一节点的指针
 	SinglyNode* next;
 };
 
 //新建节点函数
-SinglyNode* NewNode_Singly(int data/*...应填入待存储数据*/, SinglyNode* Before)	//该函数将在Before节点后插入新节点
+SinglyNode* NewNode_Singly(DataSpace data, SinglyNode* Before)	//该函数将在 Before节点后 插入新节点
 {
 	SinglyNode* NewNode = (SinglyNode*)calloc(1, sizeof(SinglyNode));
 
 	//以下代码将数据存入新节点
-	NewNode->num = data;
+	NewNode->Data = data;
 
-	NewNode->next = Before->next;
-	Before->next = NewNode;
+	if (Before)
+	{
+		NewNode->next = Before->next;
+		Before->next = NewNode;
+	}
 
 	return NewNode;
 }
@@ -42,12 +55,130 @@ void DeleteNode(SinglyNode* NodeToDelete)	//该函数将删除NodeToDelete节点
 	free(NextNode);
 }
 
-//排序函数
-void ListSort(SinglyNode* Head, int SortOrder)
-{
+void SinglyListSort(SinglyNode* Start, int count, int SortOrder)
+{//归并排序，使用递归结构
+	if (!count || count == 1) return;
 
+	int Count1 = count / 2;
+	int Count2 = count - Count1;
+
+	SinglyNode* Start1 = Start;
+	SinglyNode* Start2 = Start1;
+	for (int i = 0; i < Count1; i++)
+	{
+		Start2 = Start2->next;
+	}
+
+	SinglyListSort(Start1, Count1, SortOrder);
+	SinglyListSort(Start2, Count2, SortOrder);
+
+	SinglyNode* Head = NULL;
+	SinglyNode* NowNode = NULL;
+
+	if (SortOrder == MAXtoMIN)
+	{//降序
+		DataSpace* Buffer = (DataSpace*)malloc(sizeof(DataSpace));
+
+		while (Count1 && Count2)
+		{
+			
+
+			if (KEY_NUM(Start1) > KEY_NUM(Start2))
+			{
+				*Buffer = Start1->Data;
+				NowNode = NewNode_Singly(*Buffer, NowNode);
+				Start1 = Start1->next;
+				Count1--;
+			}
+			else
+			{
+				*Buffer = Start2->Data;
+				NowNode = NewNode_Singly(*Buffer, NowNode);
+				Start2 = Start2->next;
+				Count2--;
+			}
+			if (!Head) Head = NowNode;
+		}
+
+		while (Count1)
+		{
+			*Buffer = Start1->Data;
+			NowNode = NewNode_Singly(*Buffer, NowNode);
+			Start1 = Start1->next;
+			Count1--;
+		}
+		while (Count2)
+		{
+			*Buffer = Start2->Data;
+			NowNode = NewNode_Singly(*Buffer, NowNode);
+			Start2 = Start2->next;
+			Count2--;
+		}
+
+		SinglyNode* NowNode_S = Start;
+		NowNode = Head;
+		for (int i = 0; i < count; i++)
+		{
+			NowNode_S->Data = NowNode->Data;
+			NowNode_S = NowNode_S->next;
+			NowNode = NowNode->next;
+		}
+
+		free(Buffer);
+	}
+	else
+	{//升序
+		DataSpace* Buffer = (DataSpace*)malloc(sizeof(DataSpace));
+
+		while (Count1 && Count2)
+		{
+
+
+			if (KEY_NUM(Start1) < KEY_NUM(Start2))
+			{
+				*Buffer = Start1->Data;
+				NowNode = NewNode_Singly(*Buffer, NowNode);
+				Start1 = Start1->next;
+				Count1--;
+			}
+			else
+			{
+				*Buffer = Start2->Data;
+				NowNode = NewNode_Singly(*Buffer, NowNode);
+				Start2 = Start2->next;
+				Count2--;
+			}
+			if (!Head) Head = NowNode;
+		}
+
+		while (Count1)
+		{
+			*Buffer = Start1->Data;
+			NowNode = NewNode_Singly(*Buffer, NowNode);
+			Start1 = Start1->next;
+			Count1--;
+		}
+		while (Count2)
+		{
+			*Buffer = Start2->Data;
+			NowNode = NewNode_Singly(*Buffer, NowNode);
+			Start2 = Start2->next;
+			Count2--;
+		}
+
+		SinglyNode* NowNode_S = Start;
+		NowNode = Head;
+		for (int i = 0; i < count; i++)
+		{
+			NowNode_S->Data = NowNode->Data;
+			NowNode_S = NowNode_S->next;
+			NowNode = NowNode->next;
+		}
+
+		free(Buffer);
+	}
+	
 }
-
 
 
 //以下为双向链表部分
@@ -56,8 +187,35 @@ typedef struct DoublyNode	//单向链表节点
 	//指向上一节点的指针
 	DoublyNode* before;
 	//数据区（请在下面定义每个节点中的数据变量）
-
+	DataSpace Data;
 	//指向下一节点的指针
 	DoublyNode* next;
 };
 
+DoublyNode* NewNode_Doubly(DataSpace data, DoublyNode* Before, DoublyNode* Next)	//该函数将在 Before节点后 或 Next节点前 或 Before与Next节点之间 插入新节点
+{
+	DoublyNode* NewNode = (DoublyNode*)calloc(1, sizeof(DoublyNode));
+
+	//以下代码将数据存入新节点
+	NewNode->Data = data;
+
+	if (Before) Before->next = NewNode;
+	NewNode->before = Before;
+	if (Next) Next->before = NewNode;
+	NewNode->next = Next;	
+
+	return NewNode;
+}
+
+void DeleteNode(DoublyNode* NodeToDelete)	//该函数将删除NodeToDelete节点
+{
+	DoublyNode* BeforeNode = NodeToDelete->before;
+	DoublyNode* NextNode = NodeToDelete->next;	
+	
+	if (BeforeNode) BeforeNode->next = NextNode;
+	if (NextNode) NextNode->before = BeforeNode;
+
+	NodeToDelete = NextNode;
+
+	free(NextNode);
+}
