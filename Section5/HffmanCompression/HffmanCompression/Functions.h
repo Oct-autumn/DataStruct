bd447eb1dd7af8893c84d64535b7b48fdfe58ae8
Oct_Autumn <53611887+Oct-autumn@users.noreply.github.com>
2021-03-05 20:23:55 +0800

    HaffmanComp
    哈夫曼压缩练习ing

--- Section5/HffmanCompression/HffmanCompression/Functions.h
+++ Section5/HffmanCompression/HffmanCompression/Functions.h
@@ -1,9 +1,17 @@
 #pragma once
 #include <string.h>
+#include <stdlib.h>
+
+typedef struct DataRecord
+{
+	unsigned short int Data;
+	unsigned int Count;
+};
 
 char FilePath_open[MAX_STRING_LONG] = { "D:\\TestFile.tes" };
 char FilePath_save[MAX_STRING_LONG] = { "D:\\TestFile.hff" };
 
+//报错函数
 void error_p(const char* msg, const char* func_name)
 {
 	setcolor(LWHITE, LRED);
@@ -17,6 +25,7 @@ void error_p(const char* msg, const char* func_name)
 	setcolor();
 }
 
+//退出函数
 void exit(FILE* FileP_open, FILE* FileP_save)
 {
 	cls();
@@ -27,6 +36,7 @@ void exit(FILE* FileP_open, FILE* FileP_save)
 	PETC(2);
 }
 
+//菜单函数
 int menu()
 {
 	cls();
@@ -52,6 +62,7 @@ L1:	printf("Please select function: ");
 	
 }
 
+//打开文件函数
 FILE* file_open()
 {
 	FILE* FileP;
@@ -74,6 +85,7 @@ FILE* file_open()
 	}
 }
 
+//保存文件函数
 FILE* file_save()
 {
 	char option;
@@ -81,23 +93,46 @@ FILE* file_save()
 
 L2:	printf("Do you want to save the compressed file at the same floder?(Y/N) ");
 	
+	getchar();
 	scanf_s("%c", &option);
 	switch (option)
 	{
 	case 'Y':case 'y'://将源文件的路径更改后缀名后存入FilePath_save中
 		strcpy_s(FilePath_save, "");
-		for (int i = 0; i < strlen(FilePath_open); i++)
+
+		for (int i = strlen(FilePath_open); i >=0; i--)//获取文件名类型分隔点的位置
 		{
-			if (FilePath_open[i] == '.') break;
-			FilePath_save[i] = FilePath_open[i];
-			FilePath_save[i + 1] = '\0';
+			if (FilePath_open[i] == '.')
+			{
+				for (int j = 0; j < i; j++)//将源路径(除了文件类型)复制到新路径中
+				{
+					FilePath_save[j] = FilePath_open[j];
+				}
+				strcat_s(FilePath_save, ".hff");//加上后缀名
+				break;
+			}
 		}		
-		strcat_s(FilePath_save, ".hff");
 		break;
 	case 'N':case 'n'://由用户输入存储路径
 L3:		printf("Pleas Enter the path of file that you want to save:(Example: D:\\TestFloder\\TestFile)\n");
 		scanf_s("%s", FilePath_save, MAX_STRING_LONG);
-		strcat_s(FilePath_save, ".hff");
+		
+		for (int i = strlen(FilePath_open); i >= 0; i--)//此层循环找到文件名的起点
+		{
+			if (FilePath_open[i] == '\\')
+			{
+				int k = strlen(FilePath_save);
+				for (int j = i; j <= strlen(FilePath_open); j++)//此层循环将源文件名拼接到到新路径后
+				{
+					if (FilePath_open[j] == '.') break;
+					FilePath_save[k] = FilePath_open[j];
+					k++;
+				}
+				FilePath_save[k] = '\0';//添加终止符，以使用strcat
+				strcat_s(FilePath_save, ".hff");
+				break;
+			}
+		}
 		break;
 	default:
 		goto L2;
@@ -128,7 +163,55 @@ L4:		printf("Do you want to retry?(Y/N) ");
 	return FileP;
 }
 
+//数据统计函数
+DataRecord* CountData(FILE* FileP, int mode)
+{
+	DataRecord* DataCount = NULL;
+	switch (mode)//由压缩模式创建相应大小的统计数组并进行初始化
+	{
+	case 4:
+		DataCount = (DataRecord*)malloc(sizeof(DataRecord) * 16);
+		for (unsigned short int i = 0; i < 16; i++)
+		{
+			DataCount[i].Data = i;
+			DataCount[i].Count = 0;
+		}
+		break;
+	case 8:
+		DataCount = (DataRecord*)malloc(sizeof(DataRecord) * 256);
+		for (unsigned short int i = 0; i < 256; i++)
+		{
+			DataCount[i].Data = i;
+			DataCount[i].Count = 0;
+		}
+		break;
+	case 16:
+		DataCount = (DataRecord*)malloc(sizeof(DataRecord) * 65536);
+		for (unsigned short int i = 0; i < 65536; i++)
+		{
+			DataCount[i].Data = i;
+			DataCount[i].Count = 0;
+		}
+		break;
+	}
+
+	while (1)
+	{
+		unsigned short int Data;
+		if (fread(&Data, mode / 4, 1, FileP) != mode / 4)
+		{
+
+		}
+
+		DataCount[Data].Count++;//借由数组的有序性快速定位并计数
+	}
+
+	
+
+	return DataCount;
+}
+
 void filecompress(FILE* File_open, FILE* File_save, int mode)
 {
-	FILE
+	
 }
